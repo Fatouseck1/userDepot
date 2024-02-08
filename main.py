ec8887c4060f5445b88af50b5208d257a32b15be
Fatou <fafaseck705@gmail.com>
2024-02-08 15:13:41 +0100

    Affichage de l'année de naissance de l'utilisateur

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-age = input("Entrez votre âge : ")
-print("Vous avez", age, "ans.")
+from datetime import datetime
+
+current_year = datetime.now().year
+birth_year = current_year - int(age)
+print("Vous êtes né(e) en", birth_year)
