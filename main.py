4a191a35da990a9c927fa0eba5c63a6af947d545
Fatou <fafaseck705@gmail.com>
2024-02-08 15:13:00 +0100

    Ajout de la demande de l'âge de l'utilisateur

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-first_name = input("Entrez votre prénom : ")
-last_name = input("Entrez votre nom : ")
-print("Bonjour", first_name, last_name)
+age = input("Entrez votre âge : ")
+print("Vous avez", age, "ans.")
